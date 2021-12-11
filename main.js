5c4a84557b9d857eb559072acd59ceda22164d7a
OmarHamdann <omar.haamdan@gmail.com>
2021-12-11 21:50:25 +0200

    retrive detalis from api

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const listCard = $(".movie-list");
+const detalis = $(".more-details-section");
 
 //API \\
 const settings = {
@@ -13,13 +14,12 @@ const settings = {
 };
 
 $.ajax(settings).done(function (response) {
-  console.log(response.anime);
+  console.log(response.anime[1]);
   const top = response.anime;
   // console.log(response.top[0].title);
 
   //add card
-  top.forEach((element) => {
-    console.log(element.image_url);
+  top.forEach((element, index) => {
     listCard.append(`<div class="movie-list-item">
       <i class="fa fa-heart"></i>
       <img class="movie-list-item-img" src="${element.image_url}" alt="" />
@@ -27,18 +27,31 @@ $.ajax(settings).done(function (response) {
       <p class="movie-list-item-desc">
       ${element.synopsis}
       </p>
-      <button class="movie-list-item-button details-btn">Details</button>
+      <button id="${index}" class="movie-list-item-button details-btn">Details</button>
     </div>`);
-    //details card
-    $(".more-details-section").hide();
+  });
 
-    $(".details-btn").click(function () {
-      listCard.hide();
-      $(".more-details-section").show();
-    });
-    $(".exit").click(function () {
-      listCard.show();
-      $(".more-details-section").hide();
-    });
+  //details card
+  $(".more-details-section").hide();
+
+  $(".details-btn").click((e) => {
+    console.log(e.target.id);
+    let index = e.target.id;
+    const data = top[e.target.id];
+    console.log(data.title);
+    $(".header h1").text(data.title);
+    $(".eps").text(data.episodes);
+    $(".date").text(data.airing_start.substring(0, 10));
+    $(".score").text(data.score);
+    $(".rank").text(++index);
+    $(".image").attr("src", data.image_url);
+
+    $(".synopsis").text(data.synopsis);
+    listCard.hide();
+    $(".more-details-section").show();
+  });
+  $(".exit").click(() => {
+    listCard.show();
+    $(".more-details-section").hide();
   });
 });
